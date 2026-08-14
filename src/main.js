375218271d13b58ae93836d11bdde6f9d6a775e7
AngryKotyara <vlad6.kr94@gmail.com>
2026-08-14 18:12:59 +0500

    Load separate auth screen layout

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@
 // Keep startup order explicit: core app first, then UI/features that observe it.
 import "./media-compression-hook.js";
 import "./resumable-media.js";
-import "./app.js?v=20260813-gear2";
+import "./app.js?v=20260814-auth2";
+import "./auth-layout.js?v=20260814-auth2";
 import "./hero-copy-polish.js";
 import "./home-fullscreen-flow.js";
 import "./home-call-buttons.js";
