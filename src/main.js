6dd7111967a26fb822c905757bf027f1b912df72
AngryKotyara <vlad6.kr94@gmail.com>
2026-08-15 10:18:03 +0500

    Enable permission checkmark animation

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import "./voice-messages-v2.js";
 import "./storage-media-hydrator.js";
 import "./chat-delete-resilience.js";
 import "./interaction-polish.js";
+import "./permission-check-animation.js?v=20260815-permissions1";
 import { initPushNotifications } from "./push-notifications.js?v=20260815-push1";
 
 if ("serviceWorker" in navigator && location.protocol === "https:") {
