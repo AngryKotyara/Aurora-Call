3efd6949462bfdf331ba2f92a7009d3e365ef4d5
AngryKotyara <vlad6.kr94@gmail.com>
2026-08-30 02:18:46 +0500

    Route authenticated traffic through same-origin session proxy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 export const config = Object.freeze({
-  rpcBaseUrl: "https://taqpirplpmjihmkztwlv.supabase.co/rest/v1/rpc/",
-  functionsBaseUrl: "https://taqpirplpmjihmkztwlv.supabase.co/functions/v1/",
+  rpcBaseUrl: "/api/rpc/",
+  functionsBaseUrl: "/api/functions/",
+  publicFunctionsBaseUrl:
+    "https://taqpirplpmjihmkztwlv.supabase.co/functions/v1/",
   supabasePublishableKey: "sb_publishable_ciRXzMnLGCYUm-u-esWIOA_v6XjUEuu",
   sessionStorageKey: "aurora_session",
   mediaPermissionStoragePrefix: "aurora_media_permissions_v1",
