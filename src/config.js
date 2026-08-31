56f600fc5852ca29321ac9c354ae52caaf453011
AngryKotyara <vlad6.kr94@gmail.com>
2026-08-31 20:56:57 +0500

    Route public registration through same-origin API

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 export const config = Object.freeze({
   rpcBaseUrl: "/api/rpc/",
   functionsBaseUrl: "/api/functions/",
-  publicFunctionsBaseUrl:
-    "https://taqpirplpmjihmkztwlv.supabase.co/functions/v1/",
-  supabasePublishableKey: "sb_publishable_ciRXzMnLGCYUm-u-esWIOA_v6XjUEuu",
+  registerEmailUrl: "/api/register-email",
   sessionStorageKey: "aurora_session",
   mediaPermissionStoragePrefix: "aurora_media_permissions_v1",
   signalPollIntervalMs: 1_200,
